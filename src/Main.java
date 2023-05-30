6b0cfbf6f2d2d15770f015ff8a49dffdc06c423a
Abdurazzoq✔ <abdurazzoqrazzoqov007@gmail.com>
2023-05-30 12:25:20 +0500

    nima gap

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("nima gap"); System.out.println("Hello world!");
         System.out.println("nima gap");
+        System.out.println("Hello world!");
+        System.out.println("nima gap");
+
     }
 }
---
+++ src/iwantcreatebrench.java
@@ -0,0 +1,9 @@
+import java.util.UUID;
+
+public class iwantcreatebrench {
+String bot ;
+String name ;
+Long Id;
+UUID id;
+
+}
